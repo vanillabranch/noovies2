032008613687d234e25ed9ae3a17b01634894178
vanillabranch <lockdown@kakao.com>
2021-11-29 00:12:21 +0900

    bottom navigation 체험
    컴포넌트 별로 디렉토리를 별도로 두어서, bottom navigation으로 합치는 연습.
    쉽고 간단하게 bottom navigation을 작성할수 있으며, 각 화면별로 나누어 작업할수 있을거같다.

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
 import React, {useState} from 'react';
 import AppLoading from 'expo-app-loading';
-import {Text,Image} from "react-native";
+import {Text, Image} from "react-native";
 import * as Font from "react-native";
 import {Ionicons} from "@expo/vector-icons";
 import {Asset} from "expo-asset";
+import {NavigationContainer} from "@react-navigation/native";
+import Tabs from "./navigation/Tabs";
 
 const loadFonts = (fonts) => fonts.map(font => Font.loadAsync(font));
 
 const loadImages = (images) => assets.map(image => {
-   if(typeof image === "string"){
-       return Image.prefetch(image);
-   }else{
-       Asset.loadAsync(image);
-   }
+    if (typeof image === "string") {
+        return Image.prefetch(image);
+    } else {
+        Asset.loadAsync(image);
+    }
 });
 
 export default function App() {
@@ -20,14 +22,20 @@ export default function App() {
     const onFinish = () => setReady(true);
     const startLoading = () => async () => {
         const fonts = loadFonts([Ionicons.font]);
-        const images = loadImages([require("./jisu.jpg"),"https://img.mimint.co.kr/bbs/2020/09/02/C2009021700508272r.jpg"]);
+        const images = loadImages([require("./jisu.jpg"), "https://img.mimint.co.kr/bbs/2020/09/02/C2009021700508272r.jpg"]);
         await Promise.all([...fonts]);
     };
 
     if (!ready) {
         return <AppLoading onFinish={onFinish} onError={console.error} startAsync={startLoading}/>
     }
-    return <Text>We are done loading!!</Text>
+    //navigator를 사용하려면 NavigationContainer 를 사용하는 것은 필수 규칙이다.
+    return (
+        <NavigationContainer>
+            <Tabs/>
+        </NavigationContainer>
+    )
+
 
 }
 
