1c7bd22cc258b8ac138e56184eed9ad4a1ae40d1
vanillabranch <lockdown@kakao.com>
2021-11-28 23:17:24 +0900

    preload방식에 관하여, startAsync대신 hook방식
    startAsync 프로퍼티를 사용하여 로딩하는 방식도 있지만, useAsset,useFont를 사용한 hook방식으로 간단하게 로드할수도 있다.

--- App.js
+++ App.js
@@ -1,27 +1,25 @@
 import React, {useState} from 'react';
 import AppLoading from 'expo-app-loading';
 import {Text,Image} from "react-native";
-import * as Font from "react-native";
+import * as Font from "expo-font";
 import {Ionicons} from "@expo/vector-icons";
-import {Asset} from "expo-asset";
+import {Asset,useAssets} from "expo-asset";
+
+
 
 export default function App() {
-    const [ready, setReady] = useState(false);
-    const onFinish = () => setReady(true);
-    const startLoading = () => async () => {
-        await Font.loadAsync(Ionicons.font);
-        //앱 로고를 로딩하거나 할때 등등 미리 받아야 할것들을 여기서 다 하자.
-        //await과 async promise는 ajax async callback셋트로 기억하자
-        await Asset.loadAsync(require("./jisu.jpg"));
-        //파일시스템에 없이 서버에서 사용을 하고자 한다면, await Image.prefetch(url)을 사용해야 한다.
-        //서버 문제생길수도 있으니 그냥 파일시스템에넣어서 쓰자.
-        await Image.prefetch("https://img.mimint.co.kr/bbs/2020/09/02/C2009021700508272r.jpg");
-    };
+    //아래와 같이 간략하게 줄일수 있다.
+    //하지만 데이터베이스 초기화나 아바타, 기타 작업등이 필요하면 경우에 따라 startAsync를 사용할수도 있다.
+    const [assets] = useAssets([require("./jisu.jpg")]);
+    const [loaded] = Font.useFonts(Ionicons.font);
+    if (!assets || !loaded) {
+        return (
+            <AppLoading
 
-    if (!ready) {
-        return <AppLoading onFinish={onFinish} onError={console.error} startAsync={startLoading}/>;
+            />
+        );
     }
-    return <Text>We are done loading!</Text>
+    return <Text>We are done loading!!</Text>
 
 }
 
