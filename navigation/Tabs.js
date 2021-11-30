47fc71fc0d487eba3c1d9aff81cb5c90abcafe45
vanillabranch <lockdown@kakao.com>
2021-11-30 23:52:31 +0900

    내비게이션 옵션에 대해
    내비게이션 전체에 설정하는 prop인 screenOption 과 탭 화면 하나하나 지정할 수 있는 options 가 있음.

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -3,14 +3,53 @@ import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import Movies from "../screens/Movies";
 import Search from "../screens/Search";
 import Tv from "../screens/Tv";
+import {MaterialCommunityIcons} from "@expo/vector-icons";
+import {View, Text} from "react-native";
 
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => (
-    <Tab.Navigator>
-        <Tab.Screen name={"Movies"} component={Movies}/>
-        <Tab.Screen name={"Tv"} component={Tv} />
-        <Tab.Screen name={"Search"} component={Search}/>
+    <Tab.Navigator
+        screenOptions={{
+            tabBarLabelPosition: 'beside-icon',
+            tabBarActiveTintColor: "red",
+            tabBarInactiveTintColor: "purple",
+            tabBarStyle: {backgroundColor: "tomato"},
+            headerTitleStyle: {color: "tomato"},
+            headerRight: () => (
+                <View>
+                    <Text>hello</Text>
+                </View>
+            )
+        }}
+    >
+        <Tab.Screen name={"Movies"} component={Movies}
+                    options={{
+                        tabBarLabel: 'Movies',
+                        tabBarIcon: ({color, size}) => (
+                            <MaterialCommunityIcons name="home" color={color} size={size}/>
+                        ),
+                        tabBarBadge: 3,
+
+                    }}
+        />
+        <Tab.Screen name={"Tv"} component={Tv}
+                    options={{
+                        tabBarLabel: 'Tv',
+                        tabBarIcon: ({color, size}) => (
+                            <MaterialCommunityIcons name="youtube-tv" color={color} size={size}/>
+                        ),
+                        tabBarBadge: "hello"
+                    }}
+        />
+        <Tab.Screen name={"Search"} component={Search}
+                    options={{
+                        tabBarLabel: 'Search',
+                        tabBarIcon: ({color, size}) => (
+                            <MaterialCommunityIcons name="home-search" color={color} size={size}/>
+                        ),
+                    }}
+        />
     </Tab.Navigator>
 )
 
