583ae5effdf2b68ac8380b0142c1886fe74f7a68
vanillabranch <lockdown@kakao.com>
2021-12-02 23:22:35 +0900

    dark모드와 light모드
    useColorScheme 을 이용하여 dark모드와 light모드 체인지 실행. 그리고 화살표 함수에서 return을쓸꺼면 중괄호, 없이할꺼면 소괄호

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -5,52 +5,66 @@ import Search from "../screens/Search";
 import Tv from "../screens/Tv";
 import {MaterialCommunityIcons} from "@expo/vector-icons";
 import {View, Text} from "react-native";
+import { useColorScheme } from 'react-native';
+import {BLUE_COLOR, DARK_GREY, LIGHT_GREY, YELLOW_COLOR} from "../color";
 
 const Tab = createBottomTabNavigator();
+//안에 return 쓰면 {}, 아니면 괄호 ()
+const Tabs = () => {
+    const isDark = useColorScheme() !== "dark";
 
-const Tabs = () => (
-    <Tab.Navigator
-        screenOptions={{
-            tabBarLabelPosition: 'beside-icon',
-            tabBarActiveTintColor: "red",
-            tabBarInactiveTintColor: "purple",
-            tabBarStyle: {backgroundColor: "tomato"},
-            headerTitleStyle: {color: "tomato"},
-            headerRight: () => (
-                <View>
-                    <Text>hello</Text>
-                </View>
-            )
-        }}
-    >
-        <Tab.Screen name={"Movies"} component={Movies}
-                    options={{
-                        tabBarLabel: 'Movies',
-                        tabBarIcon: ({color, size}) => (
-                            <MaterialCommunityIcons name="home" color={color} size={size}/>
-                        ),
-                        tabBarBadge: 3,
+    return (
+        <Tab.Navigator
+            screenOptions={{
+                tabBarLabelPosition: 'beside-icon',
+                tabBarStyle: {backgroundColor: isDark ? BLUE_COLOR : "white"},
+                tabBarActiveTintColor: isDark? YELLOW_COLOR:BLUE_COLOR,
+                tabBarInactiveTintColor: isDark ? DARK_GREY : LIGHT_GREY,
+                headerTitleStyle: {
+                    color: isDark? "white":BLUE_COLOR
+                },
+                headerStyle:{
+                  backgroundColor : isDark ? BLUE_COLOR : "white"
+                },
+                headerTitleAlign: 'center',
+                headerRight: () => {
+                    return (
+                    <View>
+                        <Text>hello</Text>
+                    </View>
+                    )
+                }
+            }}
+        >
+            <Tab.Screen name={"Movies"} component={Movies}
+                        options={{
+                            tabBarLabel: 'Movies',
+                            tabBarIcon: ({color, size}) => (
+                                <MaterialCommunityIcons name="home" color={color} size={size}/>
+                            ),
+                            tabBarBadge: 3,
 
-                    }}
-        />
-        <Tab.Screen name={"Tv"} component={Tv}
-                    options={{
-                        tabBarLabel: 'Tv',
-                        tabBarIcon: ({color, size}) => (
-                            <MaterialCommunityIcons name="youtube-tv" color={color} size={size}/>
-                        ),
-                        tabBarBadge: "hello"
-                    }}
-        />
-        <Tab.Screen name={"Search"} component={Search}
-                    options={{
-                        tabBarLabel: 'Search',
-                        tabBarIcon: ({color, size}) => (
-                            <MaterialCommunityIcons name="home-search" color={color} size={size}/>
-                        ),
-                    }}
-        />
-    </Tab.Navigator>
-)
+                        }}
+            />
+            <Tab.Screen name={"Tv"} component={Tv}
+                        options={{
+                            tabBarLabel: 'Tv',
+                            tabBarIcon: ({color, size}) => (
+                                <MaterialCommunityIcons name="youtube-tv" color={color} size={size}/>
+                            ),
+                            tabBarBadge: "1"
+                        }}
+            />
+            <Tab.Screen name={"Search"} component={Search}
+                        options={{
+                            tabBarLabel: 'Search',
+                            tabBarIcon: ({color, size}) => (
+                                <MaterialCommunityIcons name="home-search" color={color} size={size}/>
+                            ),
+                        }}
+            />
+        </Tab.Navigator>
+    );
+};
 
 export default Tabs;
