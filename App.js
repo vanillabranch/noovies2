59664decac4073015783b2a97b4826ec3c82f41e
vanillabranch <lockdown@kakao.com>
2021-12-02 23:54:50 +0900

    navigation icon
    bottom navigation의 아이콘 설정하는 방법과, 이벤트별 설정하는 방법 등.

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React, {useState} from 'react';
 import AppLoading from 'expo-app-loading';
-import {Text, Image} from "react-native";
+import {Text, Image, useColorScheme} from "react-native";
 import * as Font from "react-native";
 import {Ionicons} from "@expo/vector-icons";
 import {Asset} from "expo-asset";
-import {NavigationContainer} from "@react-navigation/native";
+import {
+    NavigationContainer,
+    DarkTheme,
+    DefaultTheme} from "@react-navigation/native";
 import Tabs from "./navigation/Tabs";
 
 const loadFonts = (fonts) => fonts.map(font => Font.loadAsync(font));
@@ -26,12 +29,18 @@ export default function App() {
         await Promise.all([...fonts]);
     };
 
+    //const isDark = useColorScheme() !== "dark";
     if (!ready) {
-        return <AppLoading onFinish={onFinish} onError={console.error} startAsync={startLoading}/>
+        return <AppLoading
+                    onFinish={onFinish}
+                    onError={console.error}
+                    startAsync={startLoading}
+                />
     }
     //navigator를 사용하려면 NavigationContainer 를 사용하는 것은 필수 규칙이다.
+    //다크모드를 간편하게 적용하고 싶다면 theme={isDark?DarkTheme:DefaultTheme} 를 추가하여 사용할수도 있따.
     return (
-        <NavigationContainer>
+        <NavigationContainer >
             <Tabs/>
         </NavigationContainer>
     )
--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -3,9 +3,9 @@ import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import Movies from "../screens/Movies";
 import Search from "../screens/Search";
 import Tv from "../screens/Tv";
-import {MaterialCommunityIcons} from "@expo/vector-icons";
+import {Ionicons, MaterialCommunityIcons} from "@expo/vector-icons";
 import {View, Text} from "react-native";
-import { useColorScheme } from 'react-native';
+import {useColorScheme} from 'react-native';
 import {BLUE_COLOR, DARK_GREY, LIGHT_GREY, YELLOW_COLOR} from "../color";
 
 const Tab = createBottomTabNavigator();
@@ -16,51 +16,64 @@ const Tabs = () => {
     return (
         <Tab.Navigator
             screenOptions={{
-                tabBarLabelPosition: 'beside-icon',
-                tabBarStyle: {backgroundColor: isDark ? BLUE_COLOR : "white"},
-                tabBarActiveTintColor: isDark? YELLOW_COLOR:BLUE_COLOR,
+                /*tabBarLabelPosition: 'beside-icon',  아이콘 옆으로 글자 붙이기*/
+                tabBarStyle: {
+                    backgroundColor: isDark ? BLUE_COLOR : "white"
+                },
+                tabBarActiveTintColor: isDark ? YELLOW_COLOR : BLUE_COLOR,
                 tabBarInactiveTintColor: isDark ? DARK_GREY : LIGHT_GREY,
                 headerTitleStyle: {
-                    color: isDark? "white":BLUE_COLOR
+                    color: isDark ? "white" : BLUE_COLOR
                 },
-                headerStyle:{
-                  backgroundColor : isDark ? BLUE_COLOR : "white"
+                headerStyle: {
+                    backgroundColor: isDark ? BLUE_COLOR : "white"
                 },
                 headerTitleAlign: 'center',
                 headerRight: () => {
                     return (
-                    <View>
-                        <Text>hello</Text>
-                    </View>
+                        <View>
+                            <Text>hello</Text>
+                        </View>
                     )
+                },
+                tabBarLabelStyle : {
+                    marginTop:-5,
+                    fontSize:10,
+                    fontWeight:"600"
                 }
             }}
         >
             <Tab.Screen name={"Movies"} component={Movies}
                         options={{
                             tabBarLabel: 'Movies',
-                            tabBarIcon: ({color, size}) => (
-                                <MaterialCommunityIcons name="home" color={color} size={size}/>
-                            ),
+                            tabBarIcon: ({focused, color, size}) => {
+                                return (
+                                    <Ionicons name={focused ? "film" : "film-outline"} size={size} color={color}/>
+                                )
+                            },
                             tabBarBadge: 3,
 
                         }}
             />
             <Tab.Screen name={"Tv"} component={Tv}
                         options={{
-                            tabBarLabel: 'Tv',
-                            tabBarIcon: ({color, size}) => (
-                                <MaterialCommunityIcons name="youtube-tv" color={color} size={size}/>
-                            ),
+                            tabBarLabel: 'TV',
+                            tabBarIcon: ({focused, color, size}) => {
+                                return (
+                                    <Ionicons name="tv" size={size} color={color}/>
+                                )
+                            },
                             tabBarBadge: "1"
                         }}
             />
             <Tab.Screen name={"Search"} component={Search}
                         options={{
                             tabBarLabel: 'Search',
-                            tabBarIcon: ({color, size}) => (
-                                <MaterialCommunityIcons name="home-search" color={color} size={size}/>
-                            ),
+                            tabBarIcon: ({focused, color, size}) => {
+                                return (
+                                    <Ionicons name={focused ? "search" : "search-outline"} size={size} color={color}/>
+                                )
+                            },
                         }}
             />
         </Tab.Navigator>
